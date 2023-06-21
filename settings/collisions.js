bfc0cd30df1523948f8bba6fb31a8908d8ecc936
Pandonymous-0x20 <52674029+Pandonymous-0x20@users.noreply.github.com>
2023-06-22 00:36:16 +0200

    Add files via upload

---
+++ settings/collisions.js
@@ -0,0 +1,20 @@
+FullScreenMario.prototype.settings.collisions = {
+    "groupNames": ["Solid", "Character"],
+    "keyGroupName": "groupType",
+    "globalCheckGenerators": {
+        "Character": FullScreenMario.prototype.generateCanThingCollide,
+        "Solid": FullScreenMario.prototype.generateCanThingCollide
+    },
+    "hitCheckGenerators": {
+        "Character": {
+            "Character": FullScreenMario.prototype.generateIsCharacterTouchingCharacter,
+            "Solid": FullScreenMario.prototype.generateIsCharacterTouchingSolid
+        }
+    },
+    "hitFunctionGenerators": {
+        "Character": {
+            "Solid": FullScreenMario.prototype.generateHitCharacterSolid,
+            "Character": FullScreenMario.prototype.generateHitCharacterCharacter
+        }
+    }
+};
---
+++ settings/events.js
@@ -0,0 +1,6 @@
+FullScreenMario.prototype.settings.events = {
+    "keyOnClassCycleStart": "onThingAdd",
+    "keyDoClassCycleStart": "placed",
+    "keyCycleCheckValidity": "alive",
+    "timingDefault": 9
+};
